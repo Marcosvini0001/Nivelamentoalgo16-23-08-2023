f09383631043cd2562cd8d66c1fb7f1dc242ed57
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-08-16 21:49:38 -0300

    calculo de horas-minutos...

--- Index.java
+++ Index.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Index {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Outro");
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe as horas: ");
+        int horas = prompt.nextInt();
+        System.out.print("Informe os minutos: ");
+        int minutos = prompt.nextInt();
+        System.out.print("Informe os segundos: ");
+        int segundos = prompt.nextInt();
+        int resultado = (horas*60*60) + (minutos*60) + segundos;
+        System.out.println("Segundos: "+resultado);
     }
 }
